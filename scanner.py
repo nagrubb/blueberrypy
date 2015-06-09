45b65a17278c16a0a2e9ac6b19f278a5b940b7ee
silent-snowman <1silentsnowman1@gmail.com>
2015-06-09 16:13:55 -0700

    Make the output of scanner better

--- scanner.py
+++ scanner.py
@@ -5,16 +5,15 @@ from blueberrypy import *
 
 class BtAdapterEvents:
   def onAdvertisementScanned(self, adv):
-     print 'Found Device'
-     print '\tBluetooth Address: {0}'.format(adv.btAddress)
-     print '\tAddress Type: {0}'.format(adv.addressType)
-     print '\tRSSI: {0}'.format(adv.rssi)
+     print 'Bluetooth Address: {0}'.format(adv.btAddress)
+     print '  Address Type: {0}'.format(adv.addressType)
+     print '  RSSI: {0}'.format(adv.rssi)
 
      if (adv.shortenedLocalName is not None):
-       print '\tShortened Local Name: {0}'.format(adv.shortenedLocalName)
+       print '  Shortened Local Name: {0}'.format(adv.shortenedLocalName)
 
      if (adv.completeLocalName is not None):
-       print '\tComplete Local Name: {0}'.format(adv.completeLocalName)
+       print '  Complete Local Name: {0}'.format(adv.completeLocalName)
 
 
 events = BtAdapterEvents()
