6784680fbcf80ce59855098f051a0ceeda129a8b
silent-snowman <1silentsnowman1@gmail.com>
2015-06-10 14:46:34 -0700

    Update example scripts

--- exampleClient.py
+++ exampleClient.py
@@ -9,9 +9,6 @@ class ExampleClient(GattClient):
   def __init__(self, address, event):
     GattClient.__init__(self, address)
     self.event = event
-    self.charOne = None
-    self.charTwo = None
-    self.charThree = None
 
   def onServicesDiscovered(self, success, attErrorCode):
     print 'onServicesDiscovered({0}, {1})'.format(success, attErrorCode);
@@ -21,11 +18,10 @@ class ExampleClient(GattClient):
 
 sys.stdout.write('Connecting Gatt Client...')
 event = threading.Event()
-client = AdmGattClient('CA:17:34:15:52:0B', event)
+client = ExampleClient('CA:17:34:15:52:0B', event)
 if client.connect():
   print 'Connected'
   event.wait()
-  time.sleep(3)
   client.disconnect()
 else:
   print 'Failed to connect'
